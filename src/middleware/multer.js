2a524d309e4abce4c4fe9e03599a5b9d2db93e21
AshokSamrat292 <cse24019@iiitkalyani.ac.in>
2026-06-20 02:00:34 +0530

    Fix multer storage destination path

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     console.log("cwd =", process.cwd()); 
-    cb(null, '../public/TEMP') ; 
+    cb(null, './public/TEMP') ; 
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
